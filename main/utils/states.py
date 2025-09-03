3317afb2ac40cc86573066ea15e1003a784d15bb
Ismoilov Azizbek <98760777+Ichitube@users.noreply.github.com>
2025-09-03 11:28:56 +0900

    Add message and clan management states

--- main/utils/states.py
+++ main/utils/states.py
@@ -9,6 +9,9 @@ class Form(StatesGroup):
 class Name(StatesGroup):
     name = State()
 
+class Mes(StatesGroup):
+    msg = State()
+
 
 class Character(StatesGroup):
     character = State()
@@ -20,3 +23,28 @@ class Universe(StatesGroup):
 
 class Promo(StatesGroup):
     promo = State()
+
+
+class ClanCreateState(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_description = State()
+
+
+class ClanInvite(StatesGroup):
+    waiting_for_user_id = State()
+
+
+class ClanKick(StatesGroup):
+    waiting_for_user_id = State()
+
+
+class ClanSetName(StatesGroup):
+    waiting_for_name = State()
+
+
+class ClanSetDescription(StatesGroup):
+    waiting_for_description = State()
+
+
+class ClanMessage(StatesGroup):
+    waiting_for_message = State()
