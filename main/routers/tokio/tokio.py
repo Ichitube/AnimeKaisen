7a93ec2ccefd68ceb9bc42958809b9d868a256d5
Ismoilov Azizbek <98760777+Ichitube@users.noreply.github.com>
2025-09-03 11:37:08 +0900

    Update tokio.py

--- main/routers/tokio/tokio.py
+++ main/routers/tokio/tokio.py
@@ -1,14 +1,14 @@
 import random
-from datetime import datetime, timedelta
 
 from aiogram import Router, F
 
-from aiogram.types import CallbackQuery, InputMediaAnimation, InputMediaPhoto, Message
+from aiogram.types import CallbackQuery, InputMediaAnimation, Message
 from aiogram.enums import ParseMode
 
 from keyboards.builders import inline_builder
 from data import mongodb
 from filters.chat_type import ChatTypeFilter
+
 
 router = Router()
 
@@ -32,12 +32,64 @@ menu = ["CgACAgIAAxkBAAIVCWXMvbya7qFOU8F85SXUu24hM5wgAAKfOwACeyZoShH4z6iUPi8kNAQ
         "CgACAgIAAx0CfstymgACIBBnE7ibsUe_hxrML0hwjTHC0jWZXQACwVgAAqiJoEhlPEBRZo_wlTYE",
         "CgACAgIAAx0CfstymgACIA5nE7iRTMe8cH8bBZgvI8ZbeAW0tAACv1gAAqiJoEi3kcjjLHSleDYE",
         "CgACAgIAAx0CfstymgACIAxnE7iJXL7xxL4a5vmYVhL3zTuYZwACvVgAAqiJoEgvOYe5dmhxFjYE",
-        "CgACAgIAAx0CfstymgACIApnE7h9jBb9jkmJNh_KJ792kiapEAACu1gAAqiJoEjYTH71QJA-TjYE"]
+        "CgACAgIAAx0CfstymgACIApnE7h9jBb9jkmJNh_KJ792kiapEAACu1gAAqiJoEjYTH71QJA-TjYE",
+        "CgACAgIAAx0CfstymgACPWxnWiFG_Ozd_Bzji7nT0z-L_ARZXwACQWcAAvbt0Eotj1TRX6hROzYE",
+        "CgACAgIAAx0CfstymgACPWpnWiFBKsoORGJWrPqbc0pYaOCJuQACQGcAAvbt0EpTkQsS84ej2TYE",
+        "CgACAgIAAx0CfstymgACPWhnWiE2nNi6fRKFBoBXLch59Lm8TQACP2cAAvbt0EqaFIv-hxthITYE",
+        "CgACAgIAAx0CfstymgACPWZnWiEd21kg7iZR5J90NSyUbaCmTgACPmcAAvbt0EpO8Nts7VfWyzYE",
+        "CgACAgIAAx0CfstymgACPWRnWiD_BXM_xIf-m9Ow90yOnXfk3AACPWcAAvbt0ErlcZtAXNwlmDYE",
+        "CgACAgIAAx0CfstymgACPWJnWiD0YmwQeePBQAZH2ovucj9wTQACPGcAAvbt0Eq_pNJLTLhWbzYE",
+        "CgACAgIAAx0CfstymgACPWBnWiDro44e852CA6p1feMaEXj4RgACO2cAAvbt0EpBlZu2nfHgCzYE",
+        "CgACAgIAAx0CfstymgACPV5nWiDeZjEomft3rYZcQDPytxg0_AACOmcAAvbt0EpOoiM0UTRHbDYE",
+        "CgACAgIAAx0CfstymgACPUxnWiCIq87zNe39KMnI8-JNZtx1EwACMWcAAvbt0Epxs4C1GbPvKDYE",
+        "CgACAgIAAx0CfstymgACPUpnWiB-NbaKrUwQ22XLu6vouMnJLAACL2cAAvbt0EriVWom9jD8YzYE",
+        "CgACAgIAAx0CfstymgACPUhnWiB2zkpEDVGHHXI0LcJB0f3sigACLmcAAvbt0EoKxaZJcjFRzjYE",
+        "CgACAgIAAx0CfstymgACPUZnWiBvTqtKwcpRBEAs2zPU6lq35QACLWcAAvbt0ErS50ZnIQgGFDYE",
+        "CgACAgIAAx0CfstymgACPURnWiBpJUOx3egkYiNGig0bBIG49gACLGcAAvbt0ErohJICkiFXZTYE",
+        "CgACAgIAAx0CfstymgACPUJnWiBZ8O38ba77vuFjMv8E02u7EwACK2cAAvbt0EqV6eyIs0E97zYE",
+        "CgACAgIAAx0CfstymgACPUBnWiBPDPiSbtOh-T4ayoELgHGjIgACKmcAAvbt0EoJn6AWWGQxvTYE",
+        "CgACAgIAAx0CfstymgACPT5nWiBCuN2Ho9onD5Ke63vQaPPjAQACKWcAAvbt0EpibjUOCvoGoDYE",
+        "CgACAgIAAx0CfstymgACPTxnWiA6Jtv1xQ4jL3paAxcrrLPCdAACKGcAAvbt0Er3UO37vyDa5DYE",
+        "CgACAgIAAx0CfstymgACPThnWiAmKGNBTzAXNNzM9DGD-DktyQACJmcAAvbt0Eo3tEKgNGF6-jYE",
+        "CgACAgIAAx0CfstymgACPTZnWiAgQQs-N5EXbi1ieI9_i-KNbAACJWcAAvbt0Eo1qzujjm2IhjYE",
+        "CgACAgIAAx0CfstymgACPTRnWiAbQeTOtBXeYqwn8_Wf3q-SxAACJGcAAvbt0ErKbpuEIuLQFzYE",
+        "CgACAgIAAx0CfstymgACPSxnWh_pEIY22PoMRKtpTzp9HpBnMwACHWcAAvbt0EoAAQqwL0u9W4E2BA",
+        "CgACAgIAAx0CfstymgACPSpnWh_bemTyeoohqmYZ_QFbRWe19QACHGcAAvbt0Er7bXBZpVUYEzYE",
+        "CgACAgIAAx0CfstymgACPShnWh_STTrjZMHdujkXJ6oBH3TjkwACG2cAAvbt0ErnZ7uphAoFAAE2BA",
+        "CgACAgIAAx0CfstymgACPSZnWh_Ki1NKworFqK9Afyha07fi3gACGmcAAvbt0EpnMJP9_PIdejYE",
+        "CgACAgIAAx0CfstymgACPSRnWh_CYcOY2uxNrMnKeWv_6pXLUwACGWcAAvbt0Eox88h6ojD9DzYE",
+        "CgACAgIAAx0CfstymgACPSJnWh-50rl67kXUSacccBosqX5_uAACGGcAAvbt0EpKv37i-gs8-jYE",
+        "CgACAgIAAx0CfstymgACPSBnWh-w0HelUkGar6Vo4ryi2bmJHAACF2cAAvbt0EqucbkeYCP17TYE",
+        "CgACAgIAAx0CfstymgACPR5nWh-gNJ7dFK1QTWjjrnXObIsMJwACFmcAAvbt0ErqSJLs8LBagTYE",
+        "CgACAgIAAx0CfstymgACPRxnWh-VQDXlFrnvivfkY7cSNcSODQACFWcAAvbt0Eq_tl1KFXCi6TYE",
+        "CgACAgIAAx0CfstymgACPRpnWh-Kn8M5V2wj1wAB07y_hW-hK4cAAhRnAAL27dBKNjyombw9a6o2BA",
+        "CgACAgIAAx0CfstymgACPRhnWh-FRtSw6tO5Ft_C-QI824MMOQACE2cAAvbt0ErPoP801LqqwjYE",
+        "CgACAgIAAx0CfstymgACPRZnWh9ujJDSxsW_aDmZLIhuMm3TAgACEWcAAvbt0EqXAzhsApsQNzYE",
+        "CgACAgIAAx0CfstymgACPRRnWh9MZ8FXoClQoSCiEU8YRIIkNQACEGcAAvbt0Eq60nsfGdiGZzYE",
+        "CgACAgIAAx0CfstymgACPRJnWh857hPoKWmZTcNwhFmCC7UEkwACD2cAAvbt0EpSfauk6fjrnDYE",
+        "CgACAgIAAx0CfstymgACPQZnWh7rxf8BZaBSb1i4WdvCK6qTBAACBGcAAvbt0ErLzslLhn7R-DYE",
+        "CgACAgIAAx0CfstymgACPPxnWh60G7fTkb9wp3MNVAnWm12CfQAC_WYAAvbt0EqqNLLhgcDqvzYE",
+        "CgACAgIAAx0CfstymgACPPhnWh6Q_MWi4JYqLpQOMwtgXF7zCQAC-2YAAvbt0Eq8LwypWVJerjYE",
+        "CgACAgIAAx0CfstymgACPPZnWh6H5gUUYNcOzZ8oumMC1JOUuAAC-mYAAvbt0Eo1PKd9BtAMqTYE",
+        "CgACAgIAAx0CfstymgACPPRnWh6Dt0fHyC_yg0sYb71OHEkFbAAC-WYAAvbt0ErNHpUYMOvB7zYE",
+        "CgACAgIAAx0CfstymgACPO5nWh5jFnCnaWGBCJW0slqasmxTUwAC9mYAAvbt0Epgn0leBeRc5DYE",
+        "CgACAgIAAx0CfstymgACPOhnWh5OfXgafyd6Qz4f1ua8L9wQEQAC82YAAvbt0Eq6BCEtaoGO_TYE",
+        "CgACAgIAAx0CfstymgACPNlnWh2Qso0ejPBx4Tm0siLjaLkn5gAC6WYAAvbt0EoYmNiXYP344TYE",
+        "CgACAgIAAx0CfstymgACPNdnWh2AtS54L6E_7LRRnEMZwTIBQAAC6GYAAvbt0EpftyRKzk2sNjYE",
+        "CgACAgIAAx0CfstymgACPM1nWh0wpYaRG3vUYhUAAVQydk9CmegAAuJmAAL27dBKou_bFw0e-Ng2BA",
+        "CgACAgIAAx0CfstymgACIAxnE7iJXL7xxL4a5vmYVhL3zTuYZwACvVgAAqiJoEgvOYe5dmhxFjYE",
+        "CgACAgIAAx0CfstymgACIA5nE7iRTMe8cH8bBZgvI8ZbeAW0tAACv1gAAqiJoEi3kcjjLHSleDYE",
+        "CgACAgIAAx0CfstymgACIBBnE7ibsUe_hxrML0hwjTHC0jWZXQACwVgAAqiJoEhlPEBRZo_wlTYE",
+        "CgACAgIAAx0CfstymgACIBJnE7ih9KNvH8o3P1Yy1rTY4o7YVQACwlgAAqiJoEiVpQSxmKi5sjYE",
+        "CgACAgIAAx0CfstymgACIBRnE7ipKamfva-CfgqsiZJ-EKMGxwACxFgAAqiJoEijvBsGD_fnpjYE",
+        "CgACAgIAAx0CfstymgACBdtlzO0rWNF9QoR6R4_5ZaHZDVb37wACakkAAsywaUpFT0CPnQYM5TQE",
+        "CgACAgIAAx0CfstymgACIAhnE7hfoRhlTEwMXl0Olo7O0N33hQACuFgAAqiJoEirg1wj-bItjTYE",
+        ]
 
 
 @router.message(
     ChatTypeFilter(chat_type=["private"]),
-    F.text == "💮 Меню"
+    F.text == "〽️ Меню"
 )
 @router.callback_query(F.data == "tokio")
 async def tokio(callback: CallbackQuery | Message):
@@ -45,22 +97,39 @@ async def tokio(callback: CallbackQuery | Message):
     account = await mongodb.get_user(user_id)
 
     money = account['account']['money']
-
+    if account["universe"] == "Allstars":
+        area = "🃏 Битва"
+        area_cb = "arena"
+    else:
+        area = "🏟️ Арена"
+        area_cb = "arena"
+    power = account['campaign']['power']
+    top_text = await mongodb.get_top10_text()
     pattern = dict(
-        caption=f"❖  💮  <b>Меню</b>"
+        caption=f"❖  〽️  <b>Меню</b> ᓚᘏᗢ"
                 f"\n── •✧✧• ────────────"
-                f"\n ❖ 🌊 Добро пожаловать в нашу уникальную вселенную, где каждый игрок вносит свой вклад в создание "
-                f"неповторимого мира"
-                f"\n\n ❖ 💫 Приглашая друзей или купив священных билетов вы поддерживаете проект "
-                f"для дальнейшего развития"
-                f"\n\n ❖ 🏵 Спасибо за вашу поддержку!"
-                f"\n── •✧✧• ────────────"
-                f"\n❃ 💴 {money} ¥",
+                f"\n🌊 Добро пожаловать в мир карт"
+                "\n<blockquote>🏟️ Аниме Арена"
+                "\n⚡️ Собирай персонажей и ⚔️ сражайся"
+                "\n🎞 Реальные гифки из аниме в боях"
+                "\n🔥 Реальные навыки из аниме"
+                "\n♟ Придумай свою стратегию"
+                "\n🌟 Зарабатывай Telegram Stars"
+                "\n🐦‍🔥 Уничтожай боссов с 🎌 кланом"
+                "\n💬 Вступай в сообщество </blockquote>"
+                # # f"\n ❖<b>🏆 Топ 5 игроков</b>"
+                # # f"\n<blockquote>{top_text}</blockquote>"
+                # f"\n<blockquote><b>💡 Примечание</b>"
+                # f"\n • 🏟️ <b>Арена</b> - существует в других вселенных, кроме ⭐️ Allstars, "
+                # f"где вы можете применять навыки 🎴 персонажей из аниме"
+                # f"\n • 🃏 <b>Битва</b> - сильно отличается от 🏟️ арены и только для вселенной ⭐️ Allstars, "
+                # f"здесь вы можете сражаться в режиме карточный битвы с 🃏 колодой карт.</blockquote>"
+                f"\n❁ 💴 {money} ¥   ❁ ⚜️ Мощь: {power}",
         parse_mode=ParseMode.HTML,
         reply_markup=inline_builder(
-            ["🎐 Баннеры", "🪪 Профиль", "🏪 Рынок", "🏠 Дом", "📜 Квесты", "🃏 Битва", "🏯 Клан"],
-            ["banner", "main_page", "store", "home", "quests", "card_battle", "clan"],
-            row_width=[1, 2, 2]
+            [area, "🪪 Профиль", "🏯 Клан 🎌", " 🐦‍🔥Босс", "⛩️ Подземелье", "🏮 Рынок", "🏠 Дом"],
+            [area_cb, "main_page", "clan", "boss", "dungeon", "store", "home"],
+            row_width=[1, 2, 2, 2]
             )
     )
 
@@ -82,13 +151,3 @@ homes_photo = {'🏠 home_1': 'CgACAgIAAxkBAAIU-2XMuzNmOsXp4JxBcGGDbpD_XENiAAJwO
                '🏠 home_4': 'CgACAgIAAx0CfstymgACBSZlxMJQZb7FFLh9iPFdSpXOklwDqQACaD4AAgrXEEpTmie8hGfs1zQE',
                '🏠 home_5': 'CgACAgIAAx0CfstymgACBdtlzO0rWNF9QoR6R4_5ZaHZDVb37wACakkAAsywaUpFT0CPnQYM5TQE'
                }
-
-
-@router.callback_query(F.data == "clan")
-async def clan(callback: CallbackQuery):
-    await callback.answer(f"❖  🏯 Кланы в разработке", show_alert=True)
-
-
-@router.callback_query(F.data == "card_battle")
-async def card_battle(callback: CallbackQuery):
-    await callback.answer(f"❖  🃏 Карточная битва в разработке", show_alert=True)
